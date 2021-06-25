739ffcacbc709b4d3c2864203c9ff60eee5ed5b1
Clemens Horn <clemens.horn@mni.thm.de>
2021-06-25 17:15:37 +0200

    fix typo
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- scripts/trusted-domain.sh
+++ scripts/trusted-domain.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-kubectl exec -it (kubectl get pods | grep nextcloud| awk '{print $1}') -- sed -i "0,/'localhost'/s//'localhost',\n    1 => '192.168.49.2'/" config/config.php
+kubectl exec -it $(kubectl get pods | grep nextcloud| awk '{print $1}') -- sed -i "0,/'localhost'/s//'localhost',\n    1 => '192.168.49.2'/" config/config.php
